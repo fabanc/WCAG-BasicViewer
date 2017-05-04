a51bac661d9bafccbd0865ef9f3504ad8bd690b6
fabanc <fabien.ancelin@gmail.com>
2017-05-04 08:36:32 -0400

    Corrected typo in configuration panel
    Corrected typo in configuration panel

--- resources/configurationPanel.js
+++ resources/configurationPanel.js
@@ -230,11 +230,11 @@
                  "value": "Enable/disable the splash screen and optionally set its content."
               },
               {
-                  "label":"<strong>Percantage of the screen covered by the splash screen.</strong>",
+                  "label":"<strong>Percentage of the screen covered by the splash screen.</strong>",
                   "fieldName":"screen_ratio",
                   "type": "int",
                   "value": "75",
-                  "tooltip":"The percantage of the screen covered by the spash screen. Must be between 25 and 100."
+                  "tooltip":"The percentage of the screen covered by the spash screen. Must be between 25 and 100."
               },
               {
                   "label":"<strong>Number of seconds the splash screen will be displayed.</strong>",
