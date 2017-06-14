5bc6e3c2f0579b578def8ba8c5c1ee9b637a6c24
Horia Tudosie <htudosie@esri.ca>
2017-06-14 15:20:37 -0400

    Accessible color for language label

--- js/main.js
+++ js/main.js
@@ -129,7 +129,7 @@ define(["dojo/ready",
                 locale: document.documentElement.lang,
                 //location: window.location,
                 languages:languages,
-                textColor:this.activeColor,
+                textColor:this.color,
                 showLabel:this.config.languageLabel
             }, dojo.byId('languageSelectNode')).startup();
         },
