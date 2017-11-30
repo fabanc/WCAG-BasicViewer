9546d72de058599d836a4f3a3f77fcb723be7d83
Horia Tudosie <htudosie@esri.ca>
2017-11-30 18:23:25 -0500

    GeoCodding in its panel

--- js/GeoCoding/GeoCoding.js
+++ js/GeoCoding/GeoCoding.js
@@ -54,7 +54,6 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
             toolbar: null, 
             header: 'pageHeader_geoCoding',
             superNavigator : null,
-            maxSearchResults: 10,
             searchMarker: './images/SearchPin1.png',
             geolocatorLabelColor: "#0000ff", // 'green'
             emptyMessage: i18n.widgets.geoCoding.noAddress,
@@ -68,8 +67,8 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
             this.widgetsInTemplate = true;
 
             this.map = defaults.map;
-            this.search = defaults.search;
-            this.maxSearchResults = defaults.maxSearchResults;
+            // this.search = defaults.search;
+            // this.maxSearchResults = defaults.maxSearchResults;
             this.searchMarker = defaults.searchMarker;
             this.geolocatorLabelColor = defaults.geolocatorLabelColor;
             this.toolbar = defaults.toolbar;
@@ -117,8 +116,7 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
                         var address = evt.address.address;
                         var infoTemplate = new InfoTemplate(
                             "<div tabindex=0>"+(i18n.widgets.geoCoding.Location+
-                                (address.Addr_type.isNonEmpty() || address.Type.isNonEmpty() 
-                                    ? 
+                                (address.Addr_type.isNonEmpty() || address.Type.isNonEmpty() ? 
                                     (' (<i>'+
                                         (address.Addr_type.isNonEmpty() ? '${Addr_type}':'')+
                                         (address.Addr_type.isNonEmpty() && address.Type.isNonEmpty() ? ' - ': '')+
@@ -140,27 +138,8 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
                             );
                         this.map.graphics.add(graphic);
 
-                        
-                        var popup = this.map.infoWindow;
-
-                        popup.set("popupWindow", true);
-                        
-                        popup.setTitle(graphic.getTitle());
-                        popup.setContent(graphic.getContent());
-                        // popup.infoWindow.show();
-
-                        //display the info window with the address information
-                        var screenPnt = this.map.toScreen(location);
-                        popup.resize(400,200);
-                        popup.show(screenPnt, this.map.getInfoWindowAnchor(screenPnt));
+                        this.contentPanel.setContent(graphic.getContent());
                     }
-                    // popup.infoWindow.show();
-                    // if(features && features !== undefined && features.length > 0) {
-                    //     popup.infoWindow.setFeatures(features);
-                    // }
-                    // else { 
-                    //     popup.infoWindow.clearFeatures();
-                    // }
                 }));
 
                 this.map.on("click", lang.hitch(this, function(evt) {
@@ -176,7 +155,15 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
         contentPanel : null,
 
         makeAddressTemplate: function(address) {
-            var result = '';
+            var result = "<div tabindex=0 style='font-weight:bold;'>"+
+                                (address.Addr_type.isNonEmpty() || address.Type.isNonEmpty() ? 
+                                    (
+                                        (address.Addr_type.isNonEmpty() ? '${Addr_type}':'')+
+                                        (address.Addr_type.isNonEmpty() && address.Type.isNonEmpty() ? ': ': '')+
+                                        (address.Type.isNonEmpty() ? '${Type}':'')
+                                        ) 
+                                    : '')+"</div><hr/>";
+
             if(address.Address.isNonEmpty()) 
                 result += "<tr tabindex=0><th>"+i18n.widgets.geoCoding.Address+"</th><td>${Address}</td></tr>";
             if(address.Block.isNonEmpty()) 
@@ -209,8 +196,6 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
                 result += "<tr tabindex=0><th>"+i18n.widgets.geoCoding.Territory+"</th><td>${Territory}</td></tr>";
             if(address.CountryCode.isNonEmpty()) 
                 result += "<tr tabindex=0><th>"+i18n.widgets.geoCoding.CountryCode+"</th><td>${CountryCode}</td></tr>";
-            // if(address.Type.isNonEmpty()) 
-            //     result += "<tr tabindex=0><th>"+i18n.widgets.geoCoding.Type+"</th><td>${Type}</td></tr>";
 
             if(result !=='') {
                 result = "<table class='addressInfo' tabindex=0>"+result+"</table>";
@@ -253,10 +238,6 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
                 "height": 30
             });
 
-            // this.infoTemplate = new InfoTemplate("Location", "<div>${Address}</div><div>${MetroArea}</div><div>${Neighborhood}</div>");
-
-            ////https://developers.arcgis.com/javascript/3/sandbox/sandbox.html?sample=popup_sidepanel
-
             this.contentPanel = new ContentPane({
                 region: "center",
                 id: "geoCodingContent",
@@ -265,159 +246,16 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
             this.contentPanel.startup();
             this.contentPanel.set("content", i18n.widgets.geoCoding.instructions);
             
-            this.geoCodingHeader = new PopupInfoHeader({
-                map: this.map,
-                toolbar: this.toolbar, 
-                header: 'pageHeader_geoCoding', 
-                id: 'geoCoding_headerId', 
-                superNavigator : this.superNavigator,
-                emptyMessage : this.emptyMessage
-            }, domConstruct.create('Div', {}, this.headerNode));
-            this.geoCodingHeader.startup();
-
-            popup.set("popupWindow", false);
-
-            this.displayPopupContent = lang.hitch(this, function (feature) {
-                if(!this.toolbar.IsToolSelected('geoCoding')) return;
-                if (feature) {
-                    this.contentPanel.set("content", feature.getContent()).then(lang.hitch(this, function() {
-                        var mainSection = query('.esriViewPopup .mainSection', dojo.byId('geoCodingContent'));
-                        if(mainSection && mainSection.length > 0) {
-                            var header = query('.header', mainSection[0]);
-                            if(header && header.length > 0) {
-                                domAttr.set(header[0], 'tabindex', 0);
-                            }
-
-                            var attrTable = query('.attrTable', mainSection[0]);
-                            if(attrTable && attrTable.length > 0) {
-                                domAttr.set(attrTable[0], 'role', 'presentation');
-                                var rows = query('tr', attrTable[0]);
-                                if(rows) {
-                                    rows.forEach(function(row) {domAttr.set(row, 'tabindex', 0);});
-                                }
-                            } 
-                            else {
-                                var description = query('[dojoattachpoint=_description]', mainSection[0]);
-                                if(description && description.length > 0) {
-                                    domAttr.set(description[0], 'tabindex', 0);
-                                }
-                            }
-
-                            var editSummarySection = query('.esriViewPopup .editSummarySection', dojo.byId('geoCodingContent'));
-                            if(editSummarySection) {
-                                var editSummary =  query('.editSummary', editSummarySection[0]);
-                                if(editSummary) {
-                                    editSummary.forEach(function(edit) { domAttr.set(edit, 'tabindex', 0);});
-                                }
-                            }
-                            var images = query('.esriViewPopup img', dojo.byId('geoCodingContent'));
-                            if(images) {
-                                images.forEach(function(img) {
-                                    var alt = domAttr.get(img, 'alt');
-                                    if(!alt) {
-                                        domAttr.set(img,'alt','');
-                                    } else {
-                                        domAttr.set(img,'tabindex',0);
-                                        if(!domAttr.get(img, 'title'))
-                                        {
-                                            domAttr.set(img,'title',alt);
-                                        }
-                                    }
-                                });
-                            }
-                        }
-                    }));
-                }
-            });
-
-            // on(popup, "SetFeatures", lang.hitch(this, function() {
-            //     console.log("SetFeatures", popup.features);
-            // }));
-
-            on(popup, "ClearFeatures", lang.hitch(this, function() {
-                this.contentPanel.set("content", i18n.widgets.geoCoding.instructions);
-                // if(this.superNavigator) {
-                //     this.superNavigator.clearZone();
-                // }
-                if(this.geoCodingHeader) {
-                    this.geoCodingHeader.setTotal(0);
-                }
-            }));
-
-            on(popup, "SelectionChange", lang.hitch(this, function() {
-                var selectedFeature = popup.getSelectedFeature();
-                if(selectedFeature && selectedFeature !== undefined) {
-                    this.displayPopupContent(selectedFeature);
-                    this.clearSearchGraphics();
-                    if(selectedFeature.infoTemplate) {
-                        var geometry = selectedFeature.geometry;
-                        if(geometry.type !== "point") {
-                            var extent = geometry.getExtent().expand(1.5);
-                            this.map.setExtent(extent);
-                        } else {
-                            this.map.centerAt(geometry);
-                            if(!selectedFeature._layer) {
-                                this.searchMarkerGrafic = new Graphic(geometry, this.searchMarker);
-                                this.map.graphics.add(this.searchMarkerGrafic);
-
-                                // this.searchLabel.setText(selectedFeature.attributes.ShortLabel);
-                                // this.searchLabelGraphic = new Graphic(geometry, this.searchLabel);
-                                // this.map.graphics.add(this.searchLabelGraphic);
-                            }
-                        }
-                    }
-                }
-            }));
-
-            // on(this.toolbar, 'updateTool', lang.hitch(this, function(name) {
-            //     if(this.superNavigator && name !== 'infoPanel') {
-            //         this.superNavigator.followTheMapMode(false);
-            //     }
-            // }));
-
-            on(dojo.byId('pageBody_geoCoding'), 'keydown', lang.hitch(this, function(ev) {
-                switch(ev.keyCode) {
-                    case 37: // <
-                        if(this.geoCodingHeader.total>1) {
-                            this.geoCodingHeader.ToPrev();
-                            ev.stopPropagation();
-                            ev.preventDefault();
-                        }
-                        break;
-                    case 39: // >
-                        if(this.geoCodingHeader.total>1) {
-                            this.geoCodingHeader.ToNext();
-                            ev.stopPropagation();
-                            ev.preventDefault();
-                        }
-                        break;
-                    case 90: // Z
-                        this.geoCodingHeader.ToZoom();
-                        ev.stopPropagation();
-                        ev.preventDefault();
-                        break;
-                    case 77: // M
-                    case 80: // P
-                        this.geoCodingHeader.ToMap();
-                        ev.stopPropagation();
-                        ev.preventDefault();
-
-                        break;
-                    case 88: // X
-                    case 67: // C
-                    case 69: // E
-                        this.geoCodingHeader.ToClear();
-                        ev.stopPropagation();
-                        ev.preventDefault();
-                        break;
-
-                }}));
+            // this.geoCodingHeader = new PopupInfoHeader({
+            //     map: this.map,
+            //     toolbar: this.toolbar, 
+            //     header: 'pageHeader_geoCoding', 
+            //     id: 'geoCoding_headerId', 
+            //     superNavigator : this.superNavigator,
+            //     emptyMessage : this.emptyMessage
+            // }, domConstruct.create('Div', {}, this.headerNode));
+            // this.geoCodingHeader.startup();
         },
-
-        // clear: function() {
-        //     this.map.infoWindow.clearFeatures();
-        //     this.map.container.focus();
-        // },
 
         clearSearchGraphics: function(){
             if(this.searchMarkerGrafic) {
