75326411a397eb96a350ba9bb4c361d735d93b6e
Horia Tudosie <htudosie@esri.ca>
2018-01-31 15:14:08 -0500

    Fix Copy in PopupInfo (on Search)

--- js/PopupInfo/PopupInfo.js
+++ js/PopupInfo/PopupInfo.js
@@ -266,7 +266,6 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
                     "<div class='hzLine'></div>"+
                     "<table class='addressInfo'>"+result+"</table>"+
                     "<span tabindex=0 class='locatorScore'>Score: ${Score}</span>"+
-                    "<a class='locatorCopy' tabindex=0 onkeydown='if(event.keyCode===13 || event.keyCode===32) onclick='\"${LongLabel}\".copyToClipboard();' title='"+i18n.widgets.geoCoding.CopyToClipboard+"'>"+i18n.widgets.geoCoding.Copy+"</span>"+
                     "</div>";
             }
             return result;
