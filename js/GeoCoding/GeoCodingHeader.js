6a9e79a501bb951ff26d1f4ed284526c8d358389
Horia Tudosie <htudosie@esri.ca>
2018-02-06 16:46:46 -0500

    cleanup

--- js/GeoCoding/GeoCodingHeader.js
+++ js/GeoCoding/GeoCodingHeader.js
@@ -201,7 +201,7 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/lang", "dojo/has", "es
 
             if(!this.addressToolTip) {
                 this.addressToolTip = domConstruct.create('div', {
-                    class:'addressToolTip'  
+                    class:'addressToolTip bg'  
                 }, 'mapDiv');
                 
                 var spikeDiv = domConstruct.create('div', {
